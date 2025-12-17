cabed23bfcbf8ffe4bcec99d05c4369f076cc67d
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2025-12-17 17:34:38 +0100

    de bug

--- js/index.js
+++ js/index.js
@@ -5,14 +5,18 @@ cards.forEach(card => {
   const toggleButton = card.querySelector('[data-js="toggle-answer"]');
   const answer = card.querySelector('[data-js="answer"]');
 
-  toggleButton.addEventListener('click', () => {
-    answer.classList.toggle('card__answer--show');
-  });
+  if (toggleButton && answer) {
+    toggleButton.addEventListener('click', () => {
+      answer.classList.toggle('card__answer--show');
+    });
+  }
 });
 
 
 const bodyElement = document.querySelector('[data-js="root"]');
 const toggleTheme = document.querySelector('[data-js="lightmode"]');
-toggleTheme.addEventListener("click", () => {
-    bodyElement.classList.toggle("light")
-});
+if (toggleTheme && bodyElement) {
+  toggleTheme.addEventListener("click", () => {
+    bodyElement.classList.toggle("light");
+  });
+}

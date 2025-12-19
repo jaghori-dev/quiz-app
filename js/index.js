ea4a36181c6b3ead554584b37452b32e21468ae2
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2025-12-19 17:02:07 +0100

    feat> add interactivity to the bookmark button, arrangednthe files and code

--- js/index.js
+++ js/index.js
@@ -4,11 +4,18 @@ const cards = document.querySelectorAll('[data-js="card"]');
 cards.forEach(card => {
   const toggleButton = card.querySelector('[data-js="toggle-answer"]');
   const answer = card.querySelector('[data-js="answer"]');
+  const bookmarkButton = card.querySelector(".card__bookmark-button")
+  const bookmarkIcon = card.querySelector(".card__bookmark-icon")
 
   if (toggleButton && answer) {
     toggleButton.addEventListener('click', () => {
       answer.classList.toggle('card__answer--show');
     });
+  }
+  if (bookmarkButton && bookmarkIcon){
+    bookmarkButton.addEventListener("click", () => {
+    bookmarkIcon.classList.toggle("active")
+  })
   }
 });
 
